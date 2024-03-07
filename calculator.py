78e1b5f9cf3e6340f07ae3a85f3b60062a001813
Aryan Pokharna <aryanpokharna@outlook.com>
2024-03-08 10:19:07 +1100

    refactoring calcultor.py

--- calculator.py
+++ calculator.py
@@ -1,95 +1,6 @@
 from datetime import datetime
 import json
 
-def organise():
-    with open('response.json', 'r') as f:
-        data = json.load(f)
-    
-    investments = data["data"]["contact"]["account"]["saverProductInstances"][0]["investments"]
-    transactions = investments["transactions"]
-
-    FY20 = {"fees": [], "application": [], "referral": [], "redemption": []} # 2019-07-01 to 2020-06-30
-    FY21 = {"fees": [], "application": [], "referral": [], "redemption": []} # 2020-07-01 to 2021-06-30
-    FY22 = {"fees": [], "application": [], "referral": [], "redemption": []} # 2021-07-01 to 2022-06-30
-    FY23 = {"fees": [], "application": [], "referral": [], "redemption": []} # 2022-07-01 to 2023-06-30
-    FY24 = {"fees": [], "application": [], "referral": [], "redemption": []} # 2023-07-01 to 2024-06-30
-    # typeName = []
-
-    allApplications = []
-    allRedemptions = []
-      
-    for t in transactions:
-
-        # if t["__typename"] not in typeName:
-        #     typeName.append(t["__typename"])
-
-        if t["status"] != "PAID":
-            continue
-        
-        if t["__typename"] == "Application":
-            allApplications.append(t)
-        
-        if t["__typename"] == "Redemption":
-            allRedemptions.append(t)
-        
-        date_str = t["unitExchange"]["unitPrice"]["effectiveDate"]
-        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-
-        if date_obj < datetime(2020, 7, 1):
-            if t["__typename"] == "AccountFee":
-                FY20["fees"].append(t)
-            elif t["__typename"] == "Application":
-                FY20["application"].append(t)
-            elif t["__typename"] == "Referral":
-                FY20["referral"].append(t)
-            elif t["__typename"] == "Redemption":
-                FY20["redemption"].append(t)
-        
-        elif date_obj < datetime(2021, 7, 1):
-            if t["__typename"] == "AccountFee":
-                FY21["fees"].append(t)
-            elif t["__typename"] == "Application":
-                FY21["application"].append(t)
-            elif t["__typename"] == "Referral":
-                FY21["referral"].append(t)
-            elif t["__typename"] == "Redemption":
-                FY21["redemption"].append(t)
-        
-        elif date_obj < datetime(2022, 7, 1):
-            if t["__typename"] == "AccountFee":
-                FY22["fees"].append(t)
-            elif t["__typename"] == "Application":
-                FY22["application"].append(t)
-            elif t["__typename"] == "Referral":
-                FY22["referral"].append(t)
-            elif t["__typename"] == "Redemption":
-                FY22["redemption"].append(t)
-        
-        elif date_obj < datetime(2023, 7, 1):
-            if t["__typename"] == "AccountFee":
-                FY23["fees"].append(t)
-            elif t["__typename"] == "Application":
-                FY23["application"].append(t)
-            elif t["__typename"] == "Referral":
-                FY23["referral"].append(t)
-            elif t["__typename"] == "Redemption":
-                FY23["redemption"].append(t)
-        
-        else:
-            if t["__typename"] == "AccountFee":
-                FY24["fees"].append(t)
-            elif t["__typename"] == "Application":
-                FY24["application"].append(t)
-            elif t["__typename"] == "Referral":
-                FY24["referral"].append(t)
-            elif t["__typename"] == "Redemption":
-                FY24["redemption"].append(t)
-    # print(typeName)
-                
-    taxCalculator(allApplications, allRedemptions)
-
-    return FY20, FY21, FY22, FY23, FY24
-
 class Transaction():
     def __init__(self, sell):
         self.s = sell
@@ -113,6 +24,41 @@ class Buy():
         self.pl = None
         self.disc = False
         self.discTotal = 0
+
+def organise():
+    with open('response.json', 'r') as f:
+        data = json.load(f)
+    
+    transactions = data["data"]["contact"]["account"]["saverProductInstances"][0]["investments"]["transactions"]
+
+    allApplications = []
+    allRedemptions = []
+    allReferrals = []
+    allFees = []
+      
+    for t in transactions:
+
+        if t["status"] != "PAID":
+            continue
+        
+        if t["__typename"] == "Application":
+            allApplications.append(t)
+        
+        if t["__typename"] == "Redemption":
+            allRedemptions.append(t)
+
+        if t["__typename"] == "Referral":
+            allReferrals.append(t)
+
+        if t["__typename"] == "AccountFee":
+            allFees.append(t)
+                
+    return allApplications, allRedemptions
+
+def date(transaction):
+    date_str = transaction["unitExchange"]["unitPrice"]["effectiveDate"]
+    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+    return date_obj 
 
 def taxCalculator(allA, allR):
 
@@ -193,7 +139,7 @@ def taxCalculator(allA, allR):
             transaction.b.append(buy)
 
     # allA remainder should be current balance.
-    report([FY20, FY21, FY22, FY23, FY24])
+    return [FY20, FY21, FY22, FY23, FY24]
 
 def report(FY_array):
     with open('transaction_report.txt', 'w') as file:
@@ -217,39 +163,8 @@ def report(FY_array):
                 file.write("{:<11}|{:5}|{:10}|{:9}|{:10}|{:8}|{:3}|{:8}\n".format("", " NET ", "", "", "", round(transaction.pl, 2), "", round(transaction.tax, 2)))
             file.write(breaker)
             file.write("{:<11}|{:5}|{:10}|{:9}|{:10}|{:8}|{:3}|{:8}\n".format("", "TOTAL", "", "", "", round(total, 2), "", round(totalCGT, 2)))
-            file.write(breaker)
+            file.write(breaker)   
 
-def date(transaction):
-    date_str = transaction["unitExchange"]["unitPrice"]["effectiveDate"]
-    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-    return date_obj
-
-def yearlySummary(FY):
-    opening = 0
-    deposit = 0
-    referral = 0
-    fee = 0
-    withdraw = 0
-    closing = 0
-
-    # Calculate FY Deposits
-    for f in FY["application"]:
-        deposit += float(f["audAmount"])
-
-    # Calculate FY Deposits
-    for f in FY["referral"]:
-        referral += float(f["audAmount"])
-    
-    # Calculate FY Fees
-    for f in FY["fees"]:
-        fee += float(f["audAmount"])
-
-    # Calculate FY Withdraws
-    for f in FY["redemption"]:
-        withdraw += float(f["audAmount"])
-
-
-    return deposit, referral, fee, withdraw
-    
-
-FY20, FY21, FY22, FY23, FY24 = organise()
+allApplications, allRedemptions = organise()
+FY_array = taxCalculator(allApplications, allRedemptions)
+report(FY_array)
